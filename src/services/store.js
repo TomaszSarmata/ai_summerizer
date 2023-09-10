202fa2fd17c276a99aa4bb4e1993a5291e3838d1
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-10 12:00:58 +0100

    configured our global store which is our global state for the application and reduced the info needed to just articleApi

--- src/services/store.js
+++ src/services/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { curryGetDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware";
+
+import { articleApi } from "./article";
 
 export const store = configureStore({
-  reducer: {},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  reducer: {
+    [articleApi.reducerPath]: articleApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(articleApi.middleware),
 });
