6783805b76f3225ff5bda1e53cbd85c7f7d36e24
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-10 13:34:54 +0100

    added sanity check to see if there is data coming back and if so we are setting it up as our newArticle

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -12,6 +12,11 @@ const Demo = () => {
 
   const handleSubmit = async (e) => {
     const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
   };
 
   return (
