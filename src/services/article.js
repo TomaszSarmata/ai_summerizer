e74c39a24b6041ac5155e23d0974a17f6d04d3cd
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-10 12:52:52 +0100

    added code snippet from rapid api to create and baseUrl

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,23 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const options = {
+    method: 'GET',
+    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+    params: {
+      url: 'https://time.com/6266679/musk-ai-open-letter/',
+      length: '3'
+    },
+    headers: {
+      'X-RapidAPI-Key': 'f23fa9576cmsh298951a07f21da7p127a2ejsn27010f20e66d',
+      'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+    }
+  };
+
 export const articleApi = createApi({
   reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: ''
+  })
   endpoints: (builder) => ({
     getSummary: builder.query({
       query: (params) => "test",
