95cbcc95931f34354142811d62987e75245396ce
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-09-10 13:07:56 +0100

    deleted the code snippet after setting our headers in prepare headers method, also replaved the key with the actual key from .env

--- src/services/article.js
+++ src/services/article.js
@@ -1,23 +1,32 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const options = {
-    method: 'GET',
-    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
-    params: {
-      url: 'https://time.com/6266679/musk-ai-open-letter/',
-      length: '3'
-    },
-    headers: {
-      'X-RapidAPI-Key': 'f23fa9576cmsh298951a07f21da7p127a2ejsn27010f20e66d',
-      'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-    }
-  };
+// const options = {
+//   method: "GET",
+//   url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
+//   params: {
+//     url: "https://time.com/6266679/musk-ai-open-letter/",
+//     length: "3",
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": "f23fa9576cmsh298951a07f21da7p127a2ejsn27010f20e66d",
+//     "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
+//   },
+// };
+
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: ''
-  })
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", rapidApiKey);
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+    },
+  }),
   endpoints: (builder) => ({
     getSummary: builder.query({
       query: (params) => "test",
